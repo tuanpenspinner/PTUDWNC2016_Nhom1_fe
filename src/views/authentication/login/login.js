188d9a284bd70e895c1a23ca4532656639cb918d
Tuan <tuanpenspinnner@gmail.com>
2020-06-26 16:58:37 +0700

    add google recaptcha in login

--- src/views/authentication/login/login.js
+++ src/views/authentication/login/login.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable lines-between-class-members */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
   Button,
   Card,
@@ -16,29 +16,40 @@ import {
   InputGroupAddon,
   InputGroupText,
   Row,
-} from 'reactstrap';
-import { authenticationCustomerActions } from '../../../actions/customer/authentication';
-import { Link } from 'react-router-dom';
+} from "reactstrap";
+import ReCAPTCHA from "react-google-recaptcha";
+import { authenticationCustomerActions } from "../../../actions/customer/authentication";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
-      checkRole: 'customer',
-      err: '',
+      username: "",
+      password: "",
+      checkRole: "customer",
+      err: "",
+      reCaptcha: null,
     };
   }
+  onChange = (value) => {
+    this.setState({
+      reCaptcha: value,
+    });
+  };
 
   handleSubmit = async (e) => {
     e.preventDefault();
-    if (this.state.username === '' || this.state.password === '') {
-      this.setState({ err: 'Vui lòng nhập username hoặc password còn trống!' });
+    if (this.state.username === "" || this.state.password === "") {
+      this.setState({ err: "Vui lòng nhập username hoặc password còn trống!" });
+    } else if (this.state.reCaptcha === null) {
+      this.setState({
+        err: "Captcha không đúng!",
+      });
     } else {
       const { loginCustomer } = this.props;
       switch (this.state.checkRole) {
-        case 'customer': {
+        case "customer": {
           await loginCustomer(
             this.state.username,
             this.state.password,
@@ -46,17 +57,21 @@ class Login extends Component {
           );
           const st = this.props;
           console.log(st);
-          if (st.accesstoken === 'err') {
-            this.setState({ err: 'Email hoặc mật khẩu không chính xác !' });
+          if (st.accesstoken === "err") {
+            this.setState({ err: "Email hoặc mật khẩu không chính xác !" });
+            window.grecaptcha.reset();
+            this.setState({
+              reCaptcha: null,
+            });
           }
           if (st.isLogin === true) {
-            window.location.href = './customer/transfer';
+            window.location.href = "./customer/transfer";
           }
           break;
         }
-        case 'employee':
+        case "employee":
           break;
-        case 'administrator':
+        case "administrator":
           break;
         default:
           break;
@@ -96,7 +111,7 @@ class Login extends Component {
                           name="username"
                           autoFocus
                           onChange={(event) => {
-                            this.setState({ err: '' });
+                            this.setState({ err: "" });
                             this.setState({ username: event.target.value });
                           }}
                         />
@@ -113,14 +128,19 @@ class Login extends Component {
                           autoComplete="current-password"
                           name="password"
                           onChange={(event) => {
-                            this.setState({ err: '' });
+                            this.setState({ err: "" });
                             this.setState({ password: event.target.value });
                           }}
                         />
                       </InputGroup>
                       <div>
-                        <p style={{ color: 'red' }}>{err}</p>
+                        <p style={{ color: "red" }}>{err}</p>
                       </div>
+                      <ReCAPTCHA
+                        className="mb-3"
+                        onChange={this.onChange}
+                        sitekey="6LfYuqkZAAAAAI0aPad73yuIsMgRd_BJ0iR3uwru"
+                      />
                       <Row>
                         <Col xs="6">
                           <Button
@@ -144,14 +164,14 @@ class Login extends Component {
                 </Card>
                 <Card
                   className="text-white bg-primary py-5 d-md-down-none"
-                  style={{ width: '44%' }}
+                  style={{ width: "44%" }}
                 >
                   <CardBody>
                     <div>
                       <h2 className="text-center">Sign in with role</h2>
                       <div
                         className="custom-controls-stacked"
-                        style={{ paddingLeft: '75px', paddingTop: '15px' }}
+                        style={{ paddingLeft: "75px", paddingTop: "15px" }}
                       >
                         <label className="custom-control custom-radio">
                           <input
@@ -160,7 +180,7 @@ class Login extends Component {
                             value="customer"
                             type="radio"
                             className="custom-control-input"
-                            checked={this.state.checkRole === 'customer'}
+                            checked={this.state.checkRole === "customer"}
                             onChange={this.handleOptionChange}
                           />
                           <span className="custom-control-indicator" />
@@ -175,7 +195,7 @@ class Login extends Component {
                             value="employee"
                             type="radio"
                             className="custom-control-input"
-                            checked={this.state.checkRole === 'employee'}
+                            checked={this.state.checkRole === "employee"}
                             onChange={this.handleOptionChange}
                           />
                           <span className="custom-control-indicator" />
@@ -190,7 +210,7 @@ class Login extends Component {
                             value="administrator"
                             type="radio"
                             className="custom-control-input"
-                            checked={this.state.checkRole === 'administrator'}
+                            checked={this.state.checkRole === "administrator"}
                             onChange={this.handleOptionChange}
                           />
                           <span className="custom-control-indicator" />
